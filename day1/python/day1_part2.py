910242c93ded02c38eba3bcb529164a54d892368
Helier Waite <helierawaite@gmail.com>
2023-12-01 09:43:53 +0000

    small tidyup

--- day1/python/day1_part2.py
+++ day1/python/day1_part2.py
@@ -1,17 +1,8 @@
 import os
 
-wordy_numbers = {
-    "one": "1", 
-    "two": "2", 
-    "three": "3", 
-    "four": "4", 
-    "five": "5", 
-    "six": "6", 
-    "seven": "7", 
-    "eight": "8", 
-    "nine": "9"
-    }
+wordy_numbers = dict(zip(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], range(1, 10)))
 
+# consumes a line from the beginning, returning a list of all digits it finds
 def line_to_list(line):
     parsed = []
     remaining_line = line
@@ -21,18 +12,15 @@ def line_to_list(line):
             remaining_line = remaining_line[1:]
             continue
 
-        # found_a_word = False
+        # wasnt a digit, check if we are at the start of a word-number
         for key in wordy_numbers:
             if remaining_line.startswith(key):
-                parsed.append(wordy_numbers[key])
+                parsed.append(str(wordy_numbers[key]))
                 break
         
         remaining_line = remaining_line[1:]
     return parsed
         
-def combine_first_and_last(lst):
-    return int(lst[0] + lst[-1])
-
 def main():
     cwd = os.path.dirname(__file__)
     
@@ -41,7 +29,7 @@ def main():
 
     all_lines_stripped = [line.strip() for line in all_lines]
     parsed_lines = [line_to_list(line) for line in all_lines_stripped]
-    combined_first_and_last_digits = [combine_first_and_last(lst) for lst in parsed_lines]
+    combined_first_and_last_digits = [int(lst[0] + lst[-1]) for lst in parsed_lines]
     print(sum(combined_first_and_last_digits))
         
 
